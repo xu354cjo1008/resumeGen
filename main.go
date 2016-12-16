94de36810d1891dc5a918a058685efffe9207a82
ChingChang Li <xu354cjo1008@gmail.com>
2016-12-16 08:53:53 +0000

    Add project, education to template

--- main.go
+++ main.go
@@ -27,9 +27,26 @@ type Experience struct {
 	Summary   string
 }
 
+type Project struct {
+	Name      string
+	Role      string
+	StartDate string
+	EndDate   string
+	Summary   string
+}
+type Education struct {
+	Location  string
+	Major     string
+	StartDate string
+	EndDate   string
+	Summary   string
+}
+
 type Resume struct {
 	Profile     Profile
 	Experiences []Experience
+	Projects    []Project
+	Educations  []Education
 }
 
 const tmpl = `<div class="col-md-6 col-sm-12">
@@ -44,7 +61,7 @@ func makeHtml(data Resume, tmpl string) error {
 		},
 	}
 
-	t, err := template.New("resume").Funcs(funcs).ParseFiles("template/index.tmpl", "template/profile.tmpl", "template/experience.tmpl")
+	t, err := template.New("resume").Funcs(funcs).ParseFiles("template/index.tmpl", "template/profile.tmpl", "template/experience.tmpl", "template/project.tmpl", "template/education.tmpl")
 	if err != nil {
 		log.Println(err)
 		return err
@@ -80,6 +97,26 @@ func main() {
 						2. SAS expander firmware development
 						3. Linux hardware crypto system integration
 						4. System performance analysis and tuning`,
+			},
+		},
+		Projects: []Project{
+			{
+				Name:      "Linux storage development and system analysis",
+				Role:      "Function developer and performance engineer",
+				StartDate: "2015.06",
+				EndDate:   "2016.10",
+				Summary: `1. SAS expander firmware development\n
+						2. SAS protocal development and analysis\n
+						3. Linux storage stack study and function development\n
+						4. Storage performance analysys and tuning\n`,
+			},
+		},
+		Educations: []Education{
+			{
+				Location:  "National Taiwan University",
+				Major:     "Electric Engineering graduate school",
+				StartDate: "2012.09",
+				EndDate:   "2014.07",
 			},
 		},
 	}
