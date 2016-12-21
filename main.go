e742e3b52497c6209798d65876fe775abe51d163
ChingChang Li <xu354cjo1008@gmail.com>
2016-12-21 07:10:42 +0000

    Add config to set default template and included template components

--- main.go
+++ main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"os"
+	"path"
 	"strings"
+
+	"github.com/spf13/viper"
 )
+
+var config struct {
+	templatePath string
+	templateFile []string
+}
+
+func configure() error {
+
+	viper.SetConfigName("app")
+	viper.AddConfigPath("config")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return errors.New("Config file not found...")
+	} else {
+		config.templatePath = viper.GetString("global.templatePath")
+		config.templateFile = viper.GetStringSlice("global.templateFile")
+	}
+
+	log.Printf("\n Read config: \n template = %s\n"+
+		"template components = %v\n",
+		config.templatePath,
+		config.templateFile)
+
+	return nil
+}
 
 func makeHtml(data Resume, tmpl string) error {
 	funcs := template.FuncMap{
@@ -16,7 +46,12 @@ func makeHtml(data Resume, tmpl string) error {
 		},
 	}
 
-	t, err := template.New("resume").Funcs(funcs).ParseFiles("template/index.tmpl", "template/profile.tmpl", "template/experience.tmpl", "template/project.tmpl", "template/education.tmpl")
+	files := make([]string, len(config.templateFile))
+	for i, f := range config.templateFile {
+		files[i] = path.Join(tmpl, f)
+	}
+
+	t, err := template.New("resume").Funcs(funcs).ParseFiles(files...)
 	if err != nil {
 		log.Println(err)
 		return err
@@ -33,10 +68,16 @@ func main() {
 	var err error
 	var file []byte
 	var result *Resume
-	tempFile := flag.String("template", "template.tmpl", "template file")
+	tempFile := flag.String("template", "example", "template file")
 	userFile := flag.String("user", "user.yaml", "user resume informance file path")
 
 	flag.Parse()
+
+	err = configure()
+	if err != nil {
+		log.Println(err)
+		os.Exit(-1)
+	}
 
 	file, err = ioutil.ReadFile(*userFile)
 	if err != nil {
@@ -47,7 +88,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err = makeHtml(*result, *tempFile)
+	err = makeHtml(*result, path.Join(config.templatePath, *tempFile))
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(-1)
